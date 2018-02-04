e2fda524fdebb38bacb8c62647f61422514bef99
BikkuMyy <maripartan@gmail.com>
2018-02-04 23:12:40 +0200

    tehtävät 3.1-3.6

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "0401234567",
+        id: 1
+    },
+    {
+        name: "Martti Tienari",
+        number: "040-123456",
+        id: 2
+    },
+    {
+        name: "Arto Järvinen",
+        number: "040-123456",
+        id: 3
+    },
+    {
+        name: "Lea Kutvonen",
+        number: "040-123456",
+        id: 4
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h2>Nothing to see here ;)<h2/>')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    const date = new Date()
+    res.send(
+        `<div>Puhelinluettelossa ${persons.length} henkilön tiedot<div>
+        <div>${date}</div>`
+    )
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(n => n.id !== id)
+
+    res.status(204).end()
+})
+
+nameExists = ({ name }) => {
+    const person = persons.find(p => p.name === name)
+    if (person === undefined) {
+        return true
+    }
+    return false
+}
+
+generateId = () => {
+    return Math.floor(Math.random() * 100)
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (body.name === undefined || body.number === undefined) {
+        return res.status(400).json({ error: 'name or number missing' })
+    } else if (nameExists(body.name)) {
+        return res.status(400).json({ error: 'name should be unique' })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+    res.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
